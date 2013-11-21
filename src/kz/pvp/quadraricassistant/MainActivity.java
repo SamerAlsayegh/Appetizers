d9694e514f5b264579b3b66199f757caf658ad43
Samer Alsayegh <Samer@PvPKillz.com>
2013-11-21 10:49:54 -0500

    Test

--- src/kz/pvp/quadraricassistant/MainActivity.java
+++ src/kz/pvp/quadraricassistant/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends Activity {
 	int myohmy = 0;
 	
 	// Hello!
-	// Another line!
+	// Another line! 
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
